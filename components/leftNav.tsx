a19bf0b30efedec524b0d4c1ed6061daa2cfd169
admin999dev <fortrialshehe@gmail.com>
2025-11-03 14:20:37 -0600

    Update leftNav.tsx

--- components/leftNav.tsx
+++ components/leftNav.tsx
@@ -16,18 +16,21 @@ const LeftNav: React.FC<LeftNavProps> = ({ globals, instanceId }) => {
   const blankCss = "block w-full pl-3.5 before:pointer-events-none before:absolute before:left-0.5 before:top-1/2 before:h-0.5 before:w-1.5 before:-translate-y-1/2 text-slate-500 before:hidden before:bg-slate-300 hover:text-slate-900 hover:before:block";
 
   return (
-    <div className="hidden lg:relative lg:block lg:flex-none">
+    <div className="hidden lg:block">
       {globals?.content?.links && (
-        <div className="sticky -ml-0.5 w-64 overflow-x-hidden pl-0.5">
+        <div className="sticky top-24 w-56 overflow-y-auto">
           <nav className="text-sm">
-            <ul className="space-y-9">
+            <ul className="space-y-8">
               {globals?.content?.links.map((item) => (
                 <li key={item.group}>
-                  <h2 className="font-display font-semibold text-slate-900">{item.group}</h2>
-                  <ul className="mt-4 space-y-4 border-slate-200">
+                  <h2 className="mb-3 text-xs font-semibold uppercase tracking-widest text-slate-900">{item.group}</h2>
+                  <ul className="space-y-2">
                     {item.links?.map((child) => (
                       <li className="relative" key={child.slug}>
-                        <Link className={pathname.endsWith(child.slug ?? "") ? selectedCss : blankCss} href={`/${instanceId}/${child.slug}`}>
+                        <Link 
+                          className={pathname.endsWith(child.slug ?? "") ? selectedCss : blankCss} 
+                          href={`/${instanceId}/${child.slug}`}
+                        >
                           {child.title}
                         </Link>
                       </li>
