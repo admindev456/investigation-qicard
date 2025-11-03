01ccd4a38d08ebaa6cfb1e33823ce32b3a9436d2
admin999dev <fortrialshehe@gmail.com>
2025-11-03 14:23:31 -0600

    Update layout.tsx

--- app/[instance]/layout.tsx
+++ app/[instance]/layout.tsx
@@ -12,10 +12,7 @@ export default async function Layout({ children, params }: { children: React.Rea
   return map?.content?.pages?.length > 0 ? (
     <>
       <SiteHeader common={common} instanceId={params.instance} clientConfig={clientConfig} />
-      <div className="container mx-auto flex flex-wrap max-w-7xl justify-center px-4 pt-6">
-        <aside className="hidden lg:block lg:w-1/4 xl:w-1/6">
-          <LeftNav globals={common?.globals} instanceId={params.instance} />
-        </aside>
+      <div className="w-full px-4 sm:px-6 md:px-8 py-6 sm:py-8 md:py-10 mx-auto max-w-6xl">
         {children}
       </div>
       <SiteFooter />
@@ -23,9 +20,9 @@ export default async function Layout({ children, params }: { children: React.Rea
   ) : (
     <div className="m-12">
       <div className={`rounded-2xl p-6 bg-blue-50`}>
-        <p className="font-display text-xl text-yellow-900 mt-0 mb-2.5">Connection Successful</p>
+        <p className="font-display text-xl text-yellow-900 mt-0 mb-2.5">No Pages Found</p>
         <div className={`text-black-800`}>
-          <p className="whitespace-pre-wrap">You have connected to the content site successfully but there are no pages. Add a page in Headlesshost and then refresh this page.</p>
+          <p className="whitespace-pre-wrap">There are currently no pages in this knowledgebase. Add some content to get started!</p>
         </div>
       </div>
     </div>
