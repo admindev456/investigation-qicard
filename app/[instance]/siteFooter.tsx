21d2d76c912ae53504e7c79ee3a2a82460ab1c59
admin999dev <fortrialshehe@gmail.com>
2025-11-24 23:02:08 -0600

    Update siteFooter.tsx

--- app/[instance]/siteFooter.tsx
+++ app/[instance]/siteFooter.tsx
@@ -1,8 +1,8 @@
 export default function SiteFooter() {
   return (
     <footer>
-      <div className="mx-auto max-w-7xl px-6 pb-8 pt-16 lg:px-8 lg:pt-28">
-        <div className="mt-16 border-t border-slate-200 pt-8"></div>
+      <div className="mx-auto max-w-7xl px-6 pb-8 pt-8 lg:px-8 lg:pt-12">
+        <div className="mt-8 border-t border-slate-200 pt-8"></div>
       </div>
     </footer>
   );
