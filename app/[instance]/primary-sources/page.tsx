32a4286708a98707e156ce5f1df2ed35d4a6ebd6
admindev456 <nshahh10@gmail.com>
2025-12-07 18:43:04 +0000

    Create page.tsx

---
+++ app/[instance]/primary-sources/page.tsx
@@ -0,0 +1,12 @@
+import VideoTranscriptPlayer from "@/components/videoTranscriptPlayer";
+
+export default function PrimarySourcesPage() {
+  return (
+    <main className="w-full">
+      <article>
+        <VideoTranscriptPlayer />
+      </article>
+    </main>
+  );
+}
+
