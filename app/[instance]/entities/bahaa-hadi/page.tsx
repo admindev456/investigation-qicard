2721eb62c031408a35654eebf2fc52505224134d
admindev456 <nshahh10@gmail.com>
2025-12-11 13:40:27 -0600

    Create page.tsx

---
+++ app/[instance]/entities/bahaa-hadi/page.tsx
@@ -0,0 +1,10 @@
+import EntityProfile from "@/components/entityProfile";
+
+export default function BahaaHadiProfilePage() {
+  return (
+    <main className="w-full">
+      <EntityProfile />
+    </main>
+  );
+}
+
