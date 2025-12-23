f10f2427b0afd169ce90d91640a5f9feb2081673
admindev456 <nshahh10@gmail.com>
2025-12-23 06:27:33 -0600

    Create page.tsx

---
+++ app/[instance]/network/page.tsx
@@ -0,0 +1,12 @@
+import NetworkTree from "@/components/sections/networkTree";
+
+export default function NetworkPage() {
+  return (
+    <main className="w-full">
+      <article>
+        <NetworkTree />
+      </article>
+    </main>
+  );
+}
+
